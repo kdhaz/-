0bd777acf5e52082cecac463f0acff0ea28e8a67
kdhaz <a1s337887@gmail.com>
2021-02-18 20:13:50 +0900

    잘못된 코드 수정

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,7 @@
 import express from 'express';
 import { createServer, Server} from 'http';
 
-<<<<<<< HEAD
 import controller from './controller'
-=======
-import controller from './controller';
->>>>>>> 7be73b4f37b7d4d1ca79bb59ef0192080f2d15c7
 
 const app = express();
 
--- src/controller/index.ts
+++ src/controller/index.ts
@@ -5,8 +5,4 @@ const router = express.Router();
 
 router.use('/schools',schoolController);
 
-<<<<<<< HEAD
 export default router;
-=======
-export default router;
->>>>>>> 7be73b4f37b7d4d1ca79bb59ef0192080f2d15c7
