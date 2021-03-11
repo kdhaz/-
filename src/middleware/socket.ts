d78637b0970b1d806cabb2ff1e9a00a02700c82b
kdhaz <a1s337887@gmail.com>
2021-03-11 22:04:04 +0900

    채팅 이벤트 타입 추가

--- src/middleware/socket.ts
+++ src/middleware/socket.ts
@@ -4,6 +4,7 @@ import websocket from 'socket.io';
 import { ChatEvent } from '../types/chat';
 
 let socket: websocket.Server;
+const messageList: string[] = [];
 
 export const initWebSocket = (server: Server): void => {
   socket = new websocket.Server(server, {
@@ -16,8 +17,10 @@ export const initWebSocket = (server: Server): void => {
     console.log('connected', client.id);
     client.join('zoom');
     client.on(ChatEvent.NEW_MESSAGE, (message: string) => {
+      messageList.push(message);
       socket.to('zoom').emit(ChatEvent.GET_MESSAGE, message);
     });
+    client.emit(ChatEvent.GET_ALL_MESSAGE, messageList);
   });
   socket.on(ChatEvent.DISCONNECT, () => {
     console.log('closed');
--- src/types/chat.ts
+++ src/types/chat.ts
@@ -3,4 +3,5 @@ export enum ChatEvent {
   DISCONNECT = 'dosconnect',
   NEW_MESSAGE = 'NEW_MESSAGE',
   GET_MESSAGE = 'GET_MESSAGE',
+  GET_ALL_MESSAGE = 'GET_ALL_MESSAGE',
 }
